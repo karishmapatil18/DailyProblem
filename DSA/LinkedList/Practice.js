9110caad24d00cfd192e8533014c239885438fe9
karishmapatil18 <karishmapatil185@gmail.com>
2023-11-19 22:09:09 +0530

    Add element at Index

--- DSA/LinkedList/Practice.js
+++ DSA/LinkedList/Practice.js
@@ -24,8 +24,58 @@ class list {
 
         if(this.head == null){  
 
-            
+            this.head = newnode;
+            return; 
         }
+
+        let current = this.head;
+
+        while(current.next != null){
+            current = current.next ;
+        }
+
+        current.next = newnode ;
+        newnode.next = null ;
+
+
+    }
+
+    size(){
+
+        let count = 0 ;
+        let current = this.head ;
+
+        while(current){
+            count ++ ;
+            current = current.next;
+        }
+        return count;
+    }
+
+
+    addAtIndex(Index,data){
+        
+        if(Index < 0 || Index > this.size()){
+            console.log("Invalid Index")
+            return;
+        }
+        
+        let newnode  = new Node(data)
+        let current = this.head;
+
+        if(Index == 0){
+            addFirst(data);
+            return;
+        }
+
+        let i = 0 ;
+        while(i < Index-1){
+            current = current.next();
+            i++ ;
+        }
+
+        newnode.next = current.next ;
+        current.next = newnode ;
 
     }
 }
